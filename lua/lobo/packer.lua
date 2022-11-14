47a53f8fd4582df9417a5b207d521cf92fcb94b9
Manoel <manoel.lobo@gmail.com>
2022-11-14 16:57:02 -0300

    color theme config

--- lua/lobo/packer.lua
+++ lua/lobo/packer.lua
@@ -8,4 +8,6 @@ vim.cmd [[packadd packer.vim]]
 
 return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim' -- Packer self management
+
+    use 'folke/tokyonight.nvim' -- Tokyo Night Theme
 end)
