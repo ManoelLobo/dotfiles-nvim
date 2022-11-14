3ee1e89ffc2baac6e261e3b31ae10ccaf515b503
Manoel <manoel.lobo@gmail.com>
2022-11-14 16:36:49 -0300

    packer config

--- lua/lobo/init.lua
+++ lua/lobo/init.lua
@@ -1,1 +1,2 @@
+require("packer")
 print("LOBO here!")
---
+++ lua/lobo/packer.lua
@@ -0,0 +1,11 @@
+local status, packer = pcall(require, "packer")
+if (not status) then
+	print("Packer is not installed")
+	return
+end
+
+vim.cmd [[packadd packer.vim]]
+
+return require('packer').startup(function(use)
+	use 'wbthomason/packer.nvim' -- Packer self management
+end)
