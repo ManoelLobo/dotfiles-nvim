1db726407a04ff3d0d2e823215eca9661b931775
Manoel <manoel.lobo@gmail.com>
2022-11-17 17:12:45 -0300

    map remapped

--- lua/lobo/remap.lua
+++ lua/lobo/remap.lua
@@ -1,4 +1,7 @@
 local nnoremap = require("lobo.keymap").nnoremap
 
-nnoremap("<leader>ex", ":Ex<CR>")
+nnoremap("<leader>xp", ":Telescope file_browser<CR>")
 nnoremap("<leader>ted", ":tabe<CR>")
+
+nnoremap("<leader>sl", ":split<CR><C-w>w")
+nnoremap("<leader>sc", ":vsplit<CR><C-w>w")
